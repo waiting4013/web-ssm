8bc67575a3a8bd56872e6956acce6a1209d409cd
李志强 <597059@ky-tech.com.cn>
2022-03-02 15:35:41 +0800

    排序111

--- src/main/java/test/CollectorsTest.java
+++ src/main/java/test/CollectorsTest.java
@@ -4,6 +4,7 @@ package test;
 import alzq.Asset;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -11,15 +12,15 @@ public class CollectorsTest {
 
     public static void main(String[] args) {
         List<Asset> assets = new ArrayList<>();
-        Asset asset1 = new Asset("餅乾", "1341", 12L, 110D);
-        Asset asset2 = new Asset("餅乾", "DAS", 14L, 1D);
-        Asset asset3 = new Asset("加油", "1341", 112L, 13D);
-        Asset asset4 = new Asset("加油1", "1342", 1L, 14D);
+        Asset asset1 = new Asset("餅乾1", "1341", 12L, 110D);
+        Asset asset2 = new Asset("餅乾2", "DAS", 14L, 1D);
+        Asset asset3 = new Asset("加油1", "加油1", 112L, 13D);
+        Asset asset4 = new Asset("加油2", "1342", 1L, 14D);
         assets.add(asset1);
         assets.add(asset2);
         assets.add(asset3);
         assets.add(asset4);
-        System.out.println(assets);
+//        System.out.println(assets);
         Map<String, Long> collect = assets.stream().collect(Collectors.groupingBy(Asset::getAssetName, Collectors.summingLong(x->{
             return x.getSize().longValue();
         })));
@@ -36,12 +37,20 @@ public class CollectorsTest {
 
         System.out.println(merged);
 
-        List<Asset> assetList = assets.stream().filter(x -> x.getAssetName().equals(x.getAssetCode())).collect(Collectors.toList());
+        List<Asset> assetList = assets.stream().filter(x -> !x.getAssetName().equals(x.getAssetCode())).collect(Collectors.toList());
+
+        assets.removeIf(x -> x.getAssetName().equals(x.getAssetCode()));
+        System.out.println(assetList);
+        System.out.println(assets);
+
         Optional<Asset> optionalAsset = assets.stream().filter(x -> "1341".equals(x.getAssetCode())).findFirst();
 
         List<Long> collect1 = assetList.stream().map(Asset::getNumber).collect(Collectors.toList());
-        System.out.println(assetList);
-        System.out.println(optionalAsset);
+
+        Map<String, Asset> collect2 = assets.stream().collect(Collectors.toMap(Asset::getAssetName, Function.identity(), (x1,x2) ->x1));
+
+//        System.out.println(assetList);
+//        System.out.println(optionalAsset);
     }
 
 }
