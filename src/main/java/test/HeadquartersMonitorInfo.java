b9424269b2fed728c8dc16041ccbd6401b0b7555
李志强 <597059@ky-tech.com.cn>
2022-05-17 14:46:02 +0800

    排序111额2我收到

--- src/main/java/test/HeadquartersMonitorInfo.java
+++ src/main/java/test/HeadquartersMonitorInfo.java
@@ -1,28 +1,7 @@
-/**************************************************************************/
-/*                                                                        */
-/* Copyright (c) 2017 KYE Company                                         */
-/* 跨越速运集团有限公司版权所有                                             */
-/*                                                                        */
-/* PROPRIETARY RIGHTS of KYE Company are involved in the                  */
-/* subject matter of this material. All manufacturing, reproduction, use, */
-/* and sales rights pertaining to this subject matter are governed by the */
-/* license agreement. The recipient of this software implicitly accepts   */
-/* the terms of the license.                                              */
-/* 本软件文档资料是跨越速运集团有限公司的资产，任何人士阅读和                 */
-/* 使用本资料必须获得相应的书面授权，承担保密责任和接受相应的法律约束。        */
-/*                                                                         */
-/**************************************************************************/
 package test;
 
 import java.io.Serializable;
 
-/**
- * 总部摄像头信息表实体类
- * 数据库表名称：headquarters_monitor_info
- * 
- * 作   者：597059
- * 创建日期：2022年1月12日
- */
 public class HeadquartersMonitorInfo implements Serializable {
     private static final long serialVersionUID = 1L;
 
