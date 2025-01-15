4a14a14295fe25b7357234260c284e1dc26acffb
zzzz <641973993@qq.com>
2025-01-16 01:18:36 +0800

    www

--- src/main/java/ssm/controller/UserCongtroller.java
+++ src/main/java/ssm/controller/UserCongtroller.java
@@ -11,7 +11,7 @@ public class UserCongtroller {
 
 
         public void user(){
-
+            //启动
             ApplicationContext applicationContext=new ClassPathXmlApplicationContext("");
             applicationContext.getBean("");
 
